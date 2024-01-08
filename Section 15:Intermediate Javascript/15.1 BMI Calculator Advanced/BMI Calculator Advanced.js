aeb57cfb467e785f6cc17f4e822dd4dbc1d19844
ItIsCiprian <ionutcipriananescu@gmail.com>
2024-01-08 22:57:06 +0200

    Update BMI Calculator Advanced.js

--- Section 15:Intermediate Javascript/15.1 BMI Calculator Advanced/BMI Calculator Advanced.js
+++ Section 15:Intermediate Javascript/15.1 BMI Calculator Advanced/BMI Calculator Advanced.js
@@ -1,9 +1,17 @@
-function interpretBMI(bmi) {
-  if (bmi < 18.5) {
-    return "Your BMI is " + bmi + ", so you are underweight.";
-  } else if (bmi >= 18.5 && bmi <= 24.9) {
-    return "Your BMI is " + bmi + ", so you have a normal weight.";
-  } else {
-    return "Your BMI is " + bmi + ", so you are overweight.";
-  }
+function bmiCalculatorAdvanced(weight, height) {
+    // Calculate BMI
+    var bmi = weight / (height * height);
+
+    // Determine the BMI category and return the corresponding message
+    if (bmi < 18.5) {
+        return "Your BMI is " + bmi.toFixed(1) + ", so you are underweight.";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        return "Your BMI is " + bmi.toFixed(1) + ", so you have a normal weight.";
+    } else {
+        return "Your BMI is " + bmi.toFixed(1) + ", so you are overweight.";
+    }
 }
+
+// Example usage of the function
+var exampleBmiMessage = bmiCalculatorAdvanced(68, 1.75);
+console.log(exampleBmiMessage);
