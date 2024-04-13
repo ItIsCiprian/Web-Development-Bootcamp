1966d19ddde570797e1878a129c1c1409834d3b0
Ionut Ciprian Anescu <ionutcipriananescu@gmail.com>
2024-04-13 21:52:47 +0300

    function update

--- Section 18:Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
+++ Section 18:Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
@@ -1,14 +1,9 @@
 var numberofDrumButtons = document.querySelectorAll(.drum).length;
 
 for (var i = 8 ); i<numberofDrumButtons; i++) {
+
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    console.log(this.innerHTML);
+    alert("I got clicked!");
+
   });
 }
-document.querySelectorAll(("drum").length)
-
-document.querySelector("button").addEventListener("click", function () {
-
-  alert("I got clicked");
-
-});
