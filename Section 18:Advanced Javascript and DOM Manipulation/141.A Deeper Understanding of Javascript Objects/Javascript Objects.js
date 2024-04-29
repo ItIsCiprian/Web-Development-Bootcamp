1f00f7ae96b2a5431da49f138d57de21736a5f61
Ionut Ciprian Anescu <ionutcipriananescu@gmail.com>
2024-04-29 22:05:30 +0300

    Objects Definition

---
+++ Section 18:Advanced Javascript and DOM Manipulation/141.A Deeper Understanding of Javascript Objects/Javascript Objects.js
@@ -0,0 +1,8 @@
+function HouseKeeper(yearsofexperience, name, cleanrepertoire) {
+  this.yearsofexperience = yearsofexperience;
+  this.name = namme;
+  this.cleanrepertoire = cleanrepertoire;
+  this.clean = function () {
+    alert("Cleaning in progress...");
+  };
+}
