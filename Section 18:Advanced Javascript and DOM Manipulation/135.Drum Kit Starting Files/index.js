dcaa8894e6adc2c780e46b24a0990b33c067baa0
Ionut Ciprian Anescu <ionutcipriananescu@gmail.com>
2024-04-30 23:01:13 +0300

    refactor and better objects definition

--- Section 18:Advanced Javascript and DOM Manipulation/135.Drum Kit Starting Files/index.js
+++ Section 18:Advanced Javascript and DOM Manipulation/135.Drum Kit Starting Files/index.js
@@ -1,54 +1,38 @@
-// Select all elements with the class 'drum' and count them
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+// Refactored JavaScript Code for Drum Kit Interaction
 
-// Loop through each drum button to attach a click event listener
-for (var i = 0; i < numberOfDrumButtons; i++) {
-  // Select the current drum button in the loop
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    // Get the inner HTML of the button that was clicked
-    var buttonInnerHTML = this.innerHTML;
-    makesound(buttonInnerHTML);
+// Cache the drum buttons to avoid querying the DOM repeatedly
+const drumButtons = document.querySelectorAll(".drum");
+
+// Attach click event listeners to each drum button
+drumButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    // Play sound based on the button's inner HTML
+    makeSound(this.innerHTML);
   });
-}
-
-document.addEventListener("keypress", function (event) {
-  makesound(event.key);
 });
 
-function makesound(key) {
-  switch (key) {
-    case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
-      break;
-    case "a":
-      var tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-      break;
-    case "s":
-      var tom3 = new Audio("sounds/tom-3.mp3");
-      tom3.play();
-      break;
-    case "d":
-      var tom4 = new Audio("sounds/tom-4.mp3");
-      tom4.play();
-      break;
-    case "j":
-      var snare = new Audio("sounds/snare.mp3");
-      snare.play();
-      break;
-    case "k":
-      var crash = new Audio("sounds/crash.mp3");
-      crash.play();
-      break;
-    case "l":
-      var kick = new Audio("sounds/kick-bass.mp3");
-      kick.play();
-      break;
+// Listen for keypress events to trigger sounds
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+});
 
-    default:
-      console.log();
-      // Optionally, handle other cases or do nothing
-      break;
+// Function to play sound based on key
+function makeSound(key) {
+  const soundMap = {
+    w: "sounds/tom-1.mp3",
+    a: "sounds/tom-2.mp3",
+    s: "sounds/tom-3.mp3",
+    d: "sounds/tom-4.mp3",
+    j: "sounds/snare.mp3",
+    k: "sounds/crash.mp3",
+    l: "sounds/kick-bass.mp3",
+  };
+
+  // Check if the key is mapped to a sound
+  if (soundMap[key]) {
+    const sound = new Audio(soundMap[key]);
+    sound.play();
+  } else {
+    console.log("Pressed key is not mapped to a drum sound.");
   }
 }
