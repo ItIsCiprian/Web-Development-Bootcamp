9cac2a1b30ba6b899de6e1851e3b1ce92594b595
Ionut Ciprian Anescu <ionutcipriananescu@gmail.com>
2024-05-13 22:38:50 +0300

    jQuerry learning update

--- Section 19:jQuery/jQuery.js
+++
@@ -1,9 +0,0 @@
-for (i = 0; i < document.querySelectorAll("button").length; i++) {
-  document.querySelectorAll("button")[i].addEventListener("click", function () {
-    document.querySelector("h1").style.color = "red";
-  });
-}
-
-$("button").click(function () {
-  $("h1").css("color", "red");
-});
