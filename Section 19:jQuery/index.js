a17ed30f0c0d7096bf53cb8bd88d1efc14007857
ItIsCiprian <ionutcipriananescu@gmail.com>
2024-08-04 21:48:06 +0300

    Update index.js

--- Section 19:jQuery/index.js
+++ Section 19:jQuery/index.js
@@ -1,3 +1,5 @@
-$("button").click(function () {
+// Attach a click event listener to all button elements
+$("button").click(function() {
+  // When any button is clicked, change the color of all h1 elements to purple
   $("h1").css("color", "purple");
 });
